7500e5ffc8f0fe00bd6ed2b7ab0d143488188355
Alvin Ortmann <aortmann@c1r16s1.42wolfsburg.de>
2025-01-06 17:07:04 +0100

    added pipex

---
+++ Pipex/pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aortmann <aortmann@student.42.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/09 14:51:06 by aortmann          #+#    #+#             */
+/*   Updated: 2024/12/09 14:51:27 by aortmann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+#endif
