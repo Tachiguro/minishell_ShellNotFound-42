2d248ca30ff515ee8c7ccb56c460c1cc9c23b106
Alvin Ortmann <aortmann@c1r10s2.42wolfsburg.de>
2025-01-15 17:23:02 +0100

    exit error fix

--- sources/exit.c
+++ sources/exit.c
@@ -6,13 +6,13 @@
 /*   By: aortmann <aortmann@student.42.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 17:03:54 by aortmann          #+#    #+#             */
-/*   Updated: 2025/01/15 17:21:48 by aortmann         ###   ########.fr       */
+/*   Updated: 2025/01/15 17:22:50 by aortmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void close_fd(void)
+void close_fds(void)
 {
 	int fd;
 
@@ -39,14 +39,14 @@ ft_exit(char **args)
 			if (args[1][i] < '0' || args[1][i] > '9')
 			{
 				printf("exit: numeric argument required\n");
-				close_fd();
+				close_fds();
 				exit(255); //255 is the largest possible exit code on many systems and is often used to signal a severe or unhandled error.
 			}
 			i++;
 		}
 		status = atoi(args[1]);
 	}
-	close_all_fds();
+	close_fds();
 	exit(status);
 }
 
